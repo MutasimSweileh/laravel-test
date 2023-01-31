828696ed7bab0d9496f3eacd4128ce9dd6a9f796
Mutasim Sweileh <mutasim.sweileh@gmail.com>
2023-01-31 23:19:02 +0200

    add function getFutureEventsWithWorkshops and passed the test.

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use App\Models\Workshop;
+use Carbon\Carbon;
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -36,82 +38,21 @@ class EventsController extends BaseController
         }
         return $eventsWithWorkshops;
     }
-
-    /* TODO: complete getFutureEventWithWorkshops so that it returns events with workshops, that have not yet started
-    Requirements:
-    - only events that have not yet started should be included
-    - the event starting time is determined by the first workshop of the event
-    - the eloquent expressions should result in maximum 3 SQL queries, no matter the amount of events
-    - Don't post process query result in PHP
-    - verify your solution with `php artisan test`
-    - do a `git commit && git push` after you are done or when the time limit is over
-
-    Hints:
-    - partial or not working answers also get graded so make sure you commit what you have
-    - join, whereIn, min, groupBy, havingRaw might be helpful
-    - in the sample data set  the event with id 1 is already in the past and should therefore be excluded
-
-    Sample response on GET /futureevents:
-    ```json
-    [
-        {
-            "id": 2,
-            "name": "Laravel convention 2021",
-            "created_at": "2021-04-20T07:01:14.000000Z",
-            "updated_at": "2021-04-20T07:01:14.000000Z",
-            "workshops": [
-                {
-                    "id": 2,
-                    "start": "2021-10-21 10:00:00",
-                    "end": "2021-10-21 18:00:00",
-                    "event_id": 2,
-                    "name": "The new Eloquent - load more with less",
-                    "created_at": "2021-04-20T07:01:14.000000Z",
-                    "updated_at": "2021-04-20T07:01:14.000000Z"
-                },
-                {
-                    "id": 3,
-                    "start": "2021-11-21 09:00:00",
-                    "end": "2021-11-21 17:00:00",
-                    "event_id": 2,
-                    "name": "AutoEx - handles exceptions 100% automatic",
-                    "created_at": "2021-04-20T07:01:14.000000Z",
-                    "updated_at": "2021-04-20T07:01:14.000000Z"
-                }
-            ]
-        },
-        {
-            "id": 3,
-            "name": "React convention 2021",
-            "created_at": "2021-04-20T07:01:14.000000Z",
-            "updated_at": "2021-04-20T07:01:14.000000Z",
-            "workshops": [
-                {
-                    "id": 4,
-                    "start": "2021-08-21 10:00:00",
-                    "end": "2021-08-21 18:00:00",
-                    "event_id": 3,
-                    "name": "#NoClass pure functional programming",
-                    "created_at": "2021-04-20T07:01:14.000000Z",
-                    "updated_at": "2021-04-20T07:01:14.000000Z"
-                },
-                {
-                    "id": 5,
-                    "start": "2021-08-21 09:00:00",
-                    "end": "2021-08-21 17:00:00",
-                    "event_id": 3,
-                    "name": "Navigating the function jungle",
-                    "created_at": "2021-04-20T07:01:14.000000Z",
-                    "updated_at": "2021-04-20T07:01:14.000000Z"
-                }
-            ]
-        }
-    ]
-    ```
-     */
-
     public function getFutureEventsWithWorkshops()
     {
-        throw new \Exception('implement in coding task 2');
+        $currentTime = now();
+        $futureWorkshops = Workshop::where('start', '>=', $currentTime)
+            ->selectRaw('min(start) as start, event_id')
+            ->groupBy('event_id')
+            ->pluck('start', 'event_id');
+        $FutureEvent = [];
+        $futureWorkshops =  Event::whereIn('id', $futureWorkshops->keys())->get();
+        foreach ($futureWorkshops as $key => $event) {
+            $workshops = Workshop::where('start', '>=', $currentTime);
+            $workshopsByEvent = $workshops->where('event_id', $event->id)->get();
+            $event->workshops = $workshopsByEvent;
+            $FutureEvent[] = $event;
+        }
+        return $FutureEvent;
     }
 }
